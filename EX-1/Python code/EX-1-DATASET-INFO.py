185aeb46c83f132eb0070cd55c1a2f1754b6fa11
Varunkumar R <varunkumar420257@gmail.com>
2026-07-05 20:54:47 +0530

    Add files via upload

---
+++ EX-1/Python code/EX-1-DATASET-INFO.py
@@ -0,0 +1,10 @@
+# Program 4: Display Dataset Information
+
+# Import the Pandas library
+import pandas as pd
+
+# Load the dataset
+df = pd.read_csv("EDA-Ex-1.csv")
+
+# Display dataset information
+df.info()
